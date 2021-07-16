073f8474e2a159abdda28e4d6274a1c8c893bdab
Thang Bui <v.thangbx@vinid.net>
2021-07-16 21:32:42 +0700

    Update properties tag

--- web/log/logging_properties.go
+++ web/log/logging_properties.go
@@ -3,7 +3,7 @@ package log
 type LoggingProperties struct {
 	Development    bool `mapstructure:"development"`
 	JsonOutputMode bool `mapstructure:"json_output_mode"`
-	CallerSkip     int  `json:"caller_skip" default:"2"`
+	CallerSkip     int  `mapstructure:"caller_skip" default:"2"`
 }
 
 func (l LoggingProperties) Prefix() string {
