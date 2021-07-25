dcfdd9dde4cc9a86f1410f294070cdc957604d13
Thang Bui <thangbx@onemount.com>
2021-07-25 22:55:54 +0700

    Add default exception

--- exception/default.go
+++ exception/default.go
@@ -3,7 +3,8 @@ package exception
 import "net/http"
 
 var (
-	BadRequest = New(http.StatusBadRequest, "Bad Request")
-	Forbidden  = New(http.StatusForbidden, "Forbidden")
-	NotFound   = New(http.StatusNotFound, "Resource Not Found")
+	BadRequest   = New(http.StatusBadRequest, "Bad Request")
+	Unauthorized = New(http.StatusUnauthorized, "Unauthorized")
+	Forbidden    = New(http.StatusForbidden, "Forbidden")
+	NotFound     = New(http.StatusNotFound, "Resource Not Found")
 )
