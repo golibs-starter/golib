5e56263fd894ae167be01a91ffa314727cc242c2
Thang Bui <thangbx@onemount.com>
2021-08-03 10:50:45 +0700

    Remove unnecessary mapstruture tag, change default for logging properties

--- web/client/http_client_properties.go
+++ web/client/http_client_properties.go
@@ -6,39 +6,39 @@ type HttpClientProperties struct {
 	// Timeout specifies a time limit for requests made by this client.
 	// The timeout includes connection time, any redirects, and reading the response body.
 	// A Timeout of zero means no timeout.
-	Timeout time.Duration `mapstructure:"timeout" default:"60s"`
+	Timeout time.Duration `default:"60s"`
 
 	// MaxIdleConns is the connection pool size,
 	// and this is the maximum connection that can be open;
 	// its default value is 100 connections, zero means no limit.
-	MaxIdleConns int `mapstructure:"max_idle_conns" default:"100"`
+	MaxIdleConns int `default:"100"`
 
 	// MaxIdleConnsPerHost is the number of connection can be allowed to open per host basic.
 	// If zero, http.Transport DefaultMaxIdleConnsPerHost is used.
-	MaxIdleConnsPerHost int `mapstructure:"max_idle_conns_per_host" default:"100"`
+	MaxIdleConnsPerHost int `default:"100"`
 
 	// MaxConnsPerHost optionally limits the total number of
 	// connections per host, including connections in the dialing,
 	// active, and idle states. On limit violation, dials will block.
 	// Zero means no limit.
-	MaxConnsPerHost int `mapstructure:"max_conns_per_host" default:"100"`
+	MaxConnsPerHost int `default:"100"`
 
 	// Proxy specify proxy url and urls will apply proxy and
 	// the requests with these urls will be requested under proxy
-	Proxy ProxyProperties `mapstructure:"proxy"`
+	Proxy ProxyProperties
 }
 
 func (h HttpClientProperties) Prefix() string {
-	return "vinid.httpclient"
+	return "application.httpClient"
 }
 
 type ProxyProperties struct {
 	// Url is proxy url. Example: http://localhost:8080
-	Url string `mapstructure:"url"`
+	Url string
 
 	// AppliedUris is the list of URIs, which will be requested under above proxy
 	// Example:
 	// 		https://example.com/path/
 	//		All URL starts with https://example.com/path/ will be request under proxy
-	AppliedUris []string `mapstructure:"applied_uris"`
+	AppliedUris []string
 }
--- web/log/logging_properties.go
+++ web/log/logging_properties.go
@@ -1,11 +1,11 @@
 package log
 
 type LoggingProperties struct {
-	Development    bool `mapstructure:"development"`
-	JsonOutputMode bool `mapstructure:"json_output_mode"`
-	CallerSkip     int  `mapstructure:"caller_skip" default:"2"`
+	Development    bool `default:"false"`
+	JsonOutputMode bool `default:"true"`
+	CallerSkip     int  `default:"2"`
 }
 
 func (l LoggingProperties) Prefix() string {
-	return "vinid.logging"
+	return "application.logging"
 }
