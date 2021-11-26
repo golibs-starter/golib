449dd18d4a8aa24a4e4da18d113a515de9dca173
Thang Bui <thangbx@onemount.com>
2021-11-27 00:25:37 +0700

    Auto init global logger for web

--- web/log/global.go
+++ web/log/global.go
@@ -1,10 +1,18 @@
 package log
 
 import (
+	"fmt"
 	"gitlab.id.vin/vincart/golib/log"
 )
 
 var global log.Logger
+
+func init() {
+	var err error
+	if global, err = log.NewLogger(&log.Options{}); err != nil {
+		panic(fmt.Errorf("init global web logger error [%v]", err))
+	}
+}
 
 // ReplaceGlobal Register a logger instance as global
 func ReplaceGlobal(logger log.Logger) {
