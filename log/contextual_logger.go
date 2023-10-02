0df6c134cb657629c62f0d5be11842e41a4c1977
Thang Bui <thangbx@onemount.com>
2023-10-02 21:49:20 +0700

    Add more document for logger

--- log/contextual_logger.go
+++ log/contextual_logger.go
@@ -5,10 +5,27 @@ import (
 )
 
 type ContextualLogger interface {
+	// Debugc formats the message according to the format specifier
+	// with some additional info in the context and logs it at debug level.
 	Debugc(ctx context.Context, template string, args ...interface{})
+
+	// Infoc formats the message according to the format specifier
+	// with some additional info in the context and logs it at info level.
 	Infoc(ctx context.Context, template string, args ...interface{})
+
+	// Warnc formats the message according to the format specifier
+	// with some additional info in the context and logs it at warn level.
 	Warnc(ctx context.Context, template string, args ...interface{})
+
+	// Errorc formats the message according to the format specifier
+	// with some additional info in the context and logs it at error level.
 	Errorc(ctx context.Context, template string, args ...interface{})
+
+	// Fatalc formats the message according to the format specifier
+	// with some additional info in the context and calls os.Exit.
 	Fatalc(ctx context.Context, template string, args ...interface{})
+
+	// Panicc formats the message according to the format specifier
+	// with some additional info in the context and panics.
 	Panicc(ctx context.Context, template string, args ...interface{})
 }
